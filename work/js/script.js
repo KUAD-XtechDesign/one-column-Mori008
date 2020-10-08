c230ed48566308a3f634e8f501b5c2d39de6ba24
Mori008 <68629358+Mori008@users.noreply.github.com>
2020-10-09 00:57:45 +0900

    なーにがいけなかったんでしょね〜

--- work/js/script.js
+++ work/js/script.js
@@ -1,13 +1,14 @@
 $(function(){
   
-    //class="img_button"の要素をクリックした時、その要素の兄弟要素のclass="content"がスライドトグル表示/非表示される
-    $(".img_button").on("click",function(){
-      $(this).siblings(".content").slideToggle();
-    })
-  
-    //class="close_button"の要素をクリックした時、その要素の親要素のclass="content"がスライドアップ非表示される
-    $(".close_button").on("click",function(){
-      $(this).parent(".content").slideUp();
-    })
-  
-  });
+  //class="img_button"の要素をクリックした時、その要素の兄弟要素のclass="content"がスライドトグル表示/非表示される
+  $(".img_button").on("click",function(){
+    $(this).siblings(".content").slideToggle();
+  })
+
+  //class="close_button"の要素をクリックした時、その要素の親要素のclass="content"がスライドアップ非表示される
+  $(".close_button").on("mouseover",function(){
+    $(this).parent(".content").slideup();
+  })
+
+
+});
